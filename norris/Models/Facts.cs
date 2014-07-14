f18baa8e6d85e2d7fa8a2c364a0056b8dc2c4f78
hrishikeshio <hrishikesh911@gmail.com>
2014-07-14 15:47:23 +0530

    added discussions

--- norris/Models/Facts.cs
+++ norris/Models/Facts.cs
@@ -12,9 +12,19 @@ namespace norris.Models
         public int Views { get; set; }
 
     }
+    public class Discussion
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public string Author { get; set; }
+        public DateTime Date { get; set; }
+        public int Views { get; set; }
 
+    }
     public class FactContext : DbContext
     {
         public DbSet<Fact> Facts { get; set; }
+        public DbSet<Discussion> Discussions { get; set; }
     }
 }
